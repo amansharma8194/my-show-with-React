d6e384db0e27f48053cdf058b62b032934eef590
amansharma8194 <amankumarsharma8194031598@gmail.com>
2021-12-25 22:09:32 +0530

    fetch api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Mainpage from '../components/Mainpage';
 
 function Home() {
-  return <Mainpage>This is home page</Mainpage>;
+  const [input, setInput] = useState('');
+
+  const OninputChange = ev => {
+    // eslint-disable-next-line
+    setInput(ev.target.value);
+  };
+  const OnSearch = async () => {
+    const r = await fetch(` https://api.tvmaze.com/search/shows?q=${input}`);
+    const resp = await r.json();
+    // eslint-disable-next-line
+    console.log(resp);
+  };
+  const onkeydown = ev => {
+    if (ev.keyCode === 13) {
+      OnSearch();
+    }
+  };
+  return (
+    <Mainpage>
+      <input
+        type="text"
+        onChange={OninputChange}
+        value={input}
+        onKeyDown={onkeydown}
+      />
+      <button type="button" onMouseEnter={OnSearch}>
+        Search
+      </button>
+    </Mainpage>
+  );
 }
 
 export default Home;
