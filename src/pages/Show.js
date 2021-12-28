c201863c224b41c308a5f54fe2a0cd862d34b080
amansharma8194 <amankumarsharma8194031598@gmail.com>
2021-12-28 12:35:43 +0530

    added show page phase 2

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,20 +5,39 @@ import { GetApiResults } from '../misc/config';
 function Show() {
   const { id } = useParams();
   const [show, SetShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    let isMounted = true;
     async function getApi() {
       const Results = await GetApiResults(
         `shows/${id}?embed[]=seasons&embed[]=cast`
-      );
-      SetShow(Results);
+      ).catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+      if (isMounted) {
+        SetShow(Results);
+        setIsLoading(false);
+      }
     }
 
     getApi();
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
-  // eslint-disable-next-line
-  console.log(show);
-  return <div>This is show page</div>;
+  if (isLoading) {
+    return <div>Data is being Loaded</div>;
+  }
+  if (error) {
+    return <div>Error Occurred:{error}</div>;
+  }
+
+  return <div>{show.id}</div>;
 }
 
 export default Show;
