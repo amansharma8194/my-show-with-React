463e6f06e10d011f4a51100a609b661cf39ae4b6
amansharma8194 <amankumarsharma8194031598@gmail.com>
2021-12-28 18:54:11 +0530

    Implemented useReducer

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,12 +1,36 @@
-import React, { useEffect, useState } from 'react';
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/function-component-definition */
+import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { GetApiResults } from '../misc/config';
 
+const Reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS': {
+      return { isLoading: false, error: null, show: action.show };
+    }
+    case 'FETCH_FAILED': {
+      return { ...prevState, isLoading: false, error: action.error };
+    }
+    default:
+      return prevState;
+  }
+};
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: null,
+};
+
 function Show() {
   const { id } = useParams();
-  const [show, SetShow] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [{ isLoading, error, show }, dispatch] = useReducer(
+    Reducer,
+    initialState
+  );
+  // const [show, SetShow] = useState(null);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [error, setError] = useState(null);
 
   useEffect(() => {
     let isMounted = true;
@@ -15,13 +39,11 @@ function Show() {
         `shows/${id}?embed[]=seasons&embed[]=cast`
       ).catch(err => {
         if (isMounted) {
-          setError(err.message);
-          setIsLoading(false);
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
         }
       });
       if (isMounted) {
-        SetShow(Results);
-        setIsLoading(false);
+        dispatch({ type: 'FETCH_SUCCESS', show: Results });
       }
     }
 
@@ -36,6 +58,8 @@ function Show() {
   if (error) {
     return <div>Error Occurred:{error}</div>;
   }
+  // eslint-disable-next-line
+  console.log(show);
 
   return <div>{show.id}</div>;
 }
