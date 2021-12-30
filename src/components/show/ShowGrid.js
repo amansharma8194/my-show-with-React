1030ae2ff4324920f15d0226baecda051ac166e9
amansharma8194 <amankumarsharma8194031598@gmail.com>
2021-12-30 12:11:40 +0530

    used starred dispatch

--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -1,22 +1,39 @@
+/* eslint-disable react/function-component-definition */
 import React from 'react';
 import ShowCard from './ShowCard';
 import NotFound from '../images/not-found.png';
 import { FlexGrid } from '../styled';
+import { useShows } from '../../misc/custom_hooks';
 
-function ShowGrid({ Data }) {
+const ShowGrid = ({ Data }) => {
+  const [starredShows, starredDispatch] = useShows();
+
   return (
     <FlexGrid>
-      {Data.map(({ show }) => (
-        <ShowCard
-          key={show.id}
-          id={show.id}
-          name={show.name}
-          image={show.image ? show.image.medium : NotFound}
-          summary={show.summary}
-        />
-      ))}
+      {Data.map(({ show }) => {
+        const isStarred = starredShows.includes(show.id);
+        const onStarClick = () => {
+          if (isStarred) {
+            starredDispatch({ type: 'REMOVE', showId: show.id });
+          } else {
+            starredDispatch({ type: 'ADD', showId: show.id });
+          }
+        };
+
+        return (
+          <ShowCard
+            key={show.id}
+            id={show.id}
+            name={show.name}
+            image={show.image ? show.image.medium : NotFound}
+            summary={show.summary}
+            onStarClick={onStarClick}
+            isStarred={isStarred}
+          />
+        );
+      })}
     </FlexGrid>
   );
-}
+};
 
 export default ShowGrid;
